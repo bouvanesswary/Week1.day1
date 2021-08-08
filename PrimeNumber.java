6ac9101f8aa2b4f0bd46a927831c577620f170c0
bouvanesswary <88605685+bouvanesswary@users.noreply.github.com>
2021-08-08 13:55:11 +0530

    Update PrimeNumber.java

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -9,39 +9,39 @@ public class PrimeNumber {
 		int prime=13;
 
 				// Declare a boolean variable flag as false
-				boolean flag=false;
+				boolean flag=true;
 				
 
 				// Iterate from 2 to half of the input
 				for(int i=2;i<=i/2;++i)
 				{				
 					// Divide the input with each for loop variable and check the remainder
-
-					//int reminder=prime%i;
+                  
+					int reminder=prime%i;
 
 					// Set the flag as true when there is no reminder
-					if(prime%i==0)
+					if(reminder!=0)
 					{
-						flag=true;	
+						flag=false;	
 						// break the iterator
 						break;
 					}
-							
+				}	
 									
 				// Check the flag (Provide a condition)
 				if(flag == true)
 				
 					// Print 'Prime' when the condition matches
-					System.out.println(prime + "Prime");
+					System.out.println(prime + " is Prime");
 				
 				else
 				
 					// Print 'Not a Prime' when the condition doesn't match 
 
-					System.out.println(prime + "Not prime number");
+					System.out.println(prime + " is Not prime number");
 				
 				}
 }
-}
+
 	
 
